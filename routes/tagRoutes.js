14b8cf1e4c8359ae35f70bfb1e5d0f7ac345c6e4
Kitorosano <rosanoesteban2@gmail.com>
2026-02-22 19:24:48 -0300

    feat: :sparkles: creado endpoint para eliminar etiquetas por id

--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { createTag } from '../controllers/tagController.js';
+import { createTag, deleteTag } from '../controllers/tagController.js';
 
 const router = Router();
 
 // POST /tags => Registra una nueva etiqueta a la DB
 router.post('/', createTag);
+
+// DELETE /tags/:id => Elimina una etiqueta de la DB
+router.delete('/:id', deleteTag);
 
 export default router;
