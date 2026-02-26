843fc024ec85b11da8901c6b69eb7a57d8bafaff
Kitorosano <rosanoesteban2@gmail.com>
2026-02-26 09:38:44 -0300

    feat: :sparkles: creado endpoint para refrescar token de autenticacion

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { login, logout } from '../controllers/authController.js';
+import { login, logout, refreshToken } from '../controllers/authController.js';
 
 const router = Router();
 
 // POST /auth/login => Iniciar sesión con las credenciales de un usuario
 router.post('/login', login);
+
+// GET /auth/token => Obtener un nuevo access token utilizando el refresh token
+router.get('/token', refreshToken);
 
 // GET /auth/logout => Cerrar sesión del usuario actual
 router.delete('/logout', logout)
