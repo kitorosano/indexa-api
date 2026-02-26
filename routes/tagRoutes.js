bb2599a673d52344b2526d15421ad4ffe6b69032
Kitorosano <rosanoesteban2@gmail.com>
2026-02-26 10:17:04 -0300

    feat: :sparkles: creado middleware para validar token de acceso

--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { createTag, deleteTag } from '../controllers/tagController.js';
+import { verifyAccessToken } from '../middleware/verifyAccessToken.js';
 
 const router = Router();
 
 // POST /tags => Registra una nueva etiqueta a la DB
-router.post('/', createTag);
+router.post('/', verifyAccessToken, createTag);
 
 // DELETE /tags/:id => Elimina una etiqueta de la DB
-router.delete('/:id', deleteTag);
+router.delete('/:id', verifyAccessToken, deleteTag);
 
 export default router;
