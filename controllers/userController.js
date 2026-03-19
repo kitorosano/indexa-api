23961fb75dbeffaaa84e839696b30a5a97cd93d1
Kitorosano <rosanoesteban2@gmail.com>
2026-03-18 23:59:12 -0300

    feat: :sparkles: added cors middleware; fix some bugs; update /health endpoint response

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import db from '../config/db.js';
 import { validateUUID } from '../utils/validations.js';
 
--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import express from 'express';
+import {
+  corsErrorHandler,
+  corsMiddleware,
+} from './middleware/corsMiddleware.js';
 import authRouter from './routes/authRoutes.js';
 import bookRouter from './routes/bookRoutes.js';
-import tagRouter from './routes/tagRoutes.js';
 import groupRouter from './routes/groupRoutes.js';
+import tagRouter from './routes/tagRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(corsMiddleware);
+app.use(corsErrorHandler);
 
-app.get('/health', function (request, response) {
-  response.status(200).send('Server is healthy');
+app.get('/health', function (req, res) {
+  res.status(200).send({ message: 'Server is healthy' });
 });
-
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/books', bookRouter);
